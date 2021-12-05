b09041942a980818e4617af32d02f71521195ca6
muhesh <vmspride7@gmail.com>
2021-12-05 13:17:34 +0530

    init: dauth package

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const baseUrl = 'https://auth.delta.nitt.edu';
+  static const authorizationEndPoint = '$baseUrl/authorize';
+  static const tokenEndPoint = '$baseUrl/api/oauth/token';
+}
